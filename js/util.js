d9f4586135515a95097708bac771fda03c7a4c11
Damir <damirchelny25@yandex.ru>
2023-05-23 22:22:46 +0300

    Refactoring module2

--- js/util.js
+++
@@ -1,8 +0,0 @@
-const equalArrays = (a, b) => {
-  if (a.length != b.length) return false;
-  for (var i = 0; i < a.length; i++)
-    if (a[i] !== b[i]) return false;
-  return true;
-}
-
-export {equalArrays}
